31a8c60a7e2c92f40a77064f080628ff1dae1915
Leo <34368653+loinnn@users.noreply.github.com>
2026-05-08 09:51:44 +0800

    Add files via upload

--- src/components/PetAvatar.tsx
+++ src/components/PetAvatar.tsx
@@ -328,7 +328,7 @@ export const PetAvatar: React.FC<PetAvatarProps> = ({
                 {leftHandItems.map((item, index) => (
                   <span 
                     key={item?.id || index}
-                    className="absolute bottom-1/2 left-1/2 -translate-x-1/2 text-6xl z-30 filter drop-shadow-[0_0_10px_rgba(255,255,255,0.4)] select-none pointer-events-none"
+                    className="absolute bottom-[35%] left-1/2 -translate-x-1/2 text-6xl z-30 filter drop-shadow-[0_0_10px_rgba(255,255,255,0.4)] select-none pointer-events-none"
                     style={{ transform: 'scaleX(-1) rotate(15deg)', transformOrigin: 'center' }}
                   >
                     {item?.image}
@@ -346,7 +346,7 @@ export const PetAvatar: React.FC<PetAvatarProps> = ({
                 {rightHandItems.map((item, index) => (
                   <span 
                     key={item?.id || index}
-                    className="absolute bottom-1/2 left-1/2 -translate-x-1/2 text-6xl z-30 filter drop-shadow-[0_0_10px_rgba(255,255,255,0.4)] select-none pointer-events-none"
+                    className="absolute bottom-[55%] left-1/2 -translate-x-1/2 text-6xl z-30 filter drop-shadow-[0_0_10px_rgba(255,255,255,0.4)] select-none pointer-events-none"
                     style={{ transform: 'rotate(15deg)', transformOrigin: 'center' }}
                   >
                     {item?.image}
